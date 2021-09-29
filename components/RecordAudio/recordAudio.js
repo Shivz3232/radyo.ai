9b2743ee2bd55064773718d2a939cd7a36874dd7
gourav-nuzpapr <gourav@nuzpapr.com>
2021-09-29 19:12:59 +0530

    Recording timer added

--- components/RecordAudio/recordAudio.js
+++ components/RecordAudio/recordAudio.js
@@ -3,18 +3,25 @@ import MicIcon from '@material-ui/icons/Mic';
 import RotateLeftIcon from '@material-ui/icons/RotateLeft';
 import PlayArrowRoundedIcon from '@material-ui/icons/PlayArrowRounded';
 import StopRoundedIcon from '@material-ui/icons/StopRounded';
+import Timer from './timer';
 
 const recordAudio = () => {
   const [showRec, setShowRec] = useState(false);
   const [audioSrc, setAudioSrc] = useState('');
   const [uploadedAudioSrc, setUploadedAudioSrc] = useState('');
-  const stopRecording = useRef();
   const [recordingOn, setRecordingOn] = useState(false);
+  const [fileUploaded, setfileUploaded] = useState(false);
+  const stopRecording = useRef();
   
   const handleChange = e => {
     const file = e.target.files[0];
-    const url = URL.createObjectURL(file);
-    setUploadedAudioSrc(url);
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setUploadedAudioSrc(url);
+      setfileUploaded(true);
+    } else {
+      setfileUploaded(false);
+    }
   };
 
   const RecordFromMic = () => {
@@ -54,12 +61,21 @@ const recordAudio = () => {
         className="w-full border-2 border-indigo-600 rounded-md px-3 py-2 focus:outline-none focus:ring-1 focus:ring-indigo-500 bg-white"
         type="file"
         accept="audio/*"
-        capture
         id="uploadedAudio"
         onChange={handleChange}
       />
-      <h3 className="text-indigo-600 font-semibold">Uploaded Audio:</h3>
-      <audio id="player2" controls src={uploadedAudioSrc}></audio>
+      {fileUploaded && (
+        <>
+          <h3 className="text-indigo-600 font-semibold">Uploaded Audio:</h3>
+          <audio
+            className="w-full"
+            id="player2"
+            controls
+            src={uploadedAudioSrc}
+          ></audio>
+        </>
+      )}
+
       <p className="text-center text-indigo-600 font-semibold">OR</p>
       <button
         className="w-full rounded-md px-3 py-2 focus:outline-none focus:ring-1 focus:ring-indigo-500 text-white bg-indigo-600 font-semibold"
@@ -72,7 +88,7 @@ const recordAudio = () => {
       </button>
       {showRec && (
         <>
-          <div className="flex mx-auto space-x-1">
+          <div className="flex mx-auto space-x-2">
             <button
               className="w-full rounded-md px-3 py-2 focus:outline-none focus:ring-1 focus:ring-indigo-500 text-white bg-indigo-600 font-semibold"
               onClick={RecordFromMic}
@@ -91,12 +107,12 @@ const recordAudio = () => {
             </button>
           </div>
           {recordingOn && (
-            <div className="text-center space-x-3">
-              <RotateLeftIcon />
+            <div className="text-center space-x-3 bg-gray-200 rounded-md">
               <MicIcon />
+              <Timer />
             </div>
           )}
-          <br />
+          
           <p className="mx-auto text-indigo-600 font-semibold ml-1">
             Recorded Audio
           </p>
@@ -114,36 +130,4 @@ const recordAudio = () => {
 };
 
 export default recordAudio;
-
-{
-  /* <label htmlFor="devices">Choose a Device: </label>
-         <select name="cars" id="cars">
-            {deviceList.map((device) => {
-               return <option value={device.deviceId}>{device.label}</option>;
-            })}
-         </select> */
-}
-
-
-
-{
-  /* <a ref={downloadLink} id="download">
-  <h2>Download</h2>
-</a>; */
-}
-
-// const downloadLink = useRef();
-// downloadLink.current.href = URL.createObjectURL(new Blob(recordedChunks));
-// downloadLink.current.download = 'testing.wav';
-
-// const MicOptions = () => {
-//   navigator.mediaDevices.enumerateDevices().then(devices => {
-//     devices = devices.filter(d => d.kind === 'audioinput');
-//     setDeviceList(devices);
-//     console.log(devices);
-//     RecordFromMic(devices);
-//   });
-// };
-
-// const [selectedDevice, setSelectedDevice] = useState({});
-// const [deviceList, setDeviceList] = useState([]);
+{/* <RotateLeftIcon /> */}
---
+++ components/RecordAudio/timer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
+import ReactStopwatch from 'react-stopwatch';
+
+const timer = () => {
+  return (
+    <ReactStopwatch
+      seconds={0}
+      minutes={0}
+      hours={0}
+      limit="00:05:00"
+      onCallback={() => console.log('Finish')}
+      render={({ formatted }) => {
+        return (
+          <>
+              {formatted}
+          </>
+        );
+      }}
+    />
+  );
+};
+
+export default timer;
