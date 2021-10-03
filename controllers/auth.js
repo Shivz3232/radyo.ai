7c7157755f71c71843302975acdc7069f3bd6236
aishnuz <91432335+aishnuz@users.noreply.github.com>
2021-10-03 13:40:13 +0530

    Create auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect, useContext, createContext } from 'react';
+import nookies from 'nookies';
+import firebaseClient from '../utils/firebase/firebaseClient';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+const AuthContext = createContext({});
+
+export const AuthProvider = ({ children }) => {
+  firebaseClient();
+  const [username, setUserName] = useState(null);
+  const [userid, setUserId] = useState(null);
+  const [useremail, setUserEmail] = useState(null);
+  useEffect(() => {
+    return firebase.auth().onIdTokenChanged(async user => {
+      if (!user) {
+        setUserName(null);
+        setUserId(null);
+        setUserEmail(null);
+        nookies.set(undefined, 'token', '', { path: '/' });
+        return;
+      }
+
+      const token = await user.getIdToken();
+      setUserName(user.displayName);
+      setUserId(user.uid);
+      setUserEmail(user.email);
+      nookies.set(undefined, 'token', token, { path: '/' });
+    });
+  }, []);
+  return (
+    <AuthContext.Provider value={{ username, userid, useremail }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
