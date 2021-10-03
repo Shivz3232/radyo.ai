95191c6fcee6994dee52b8a19d0e5672ad8df04c
aishnuz <91432335+aishnuz@users.noreply.github.com>
2021-10-03 13:38:20 +0530

    Update HeaderLoginBtn.jsx

--- components/Header/HeaderLoginBtn.jsx
+++ components/Header/HeaderLoginBtn.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import HeaderAvatar from './HeaderAvatar';
 import UserMenu from './UserMenu';
+import { useAuth } from '../../controllers/auth';
 
 const HeaderLoginBtn = ({ data }) => {
+  const { userid } = useAuth();
   function handleClick() {}
-  return data.loggedIn ? (
+  return userid ? (
     <UserMenu data={data} />
   ) : (
     <div
       onClick={handleClick}
       className="border border-indigo-650 rounded px-4 py-2 bg-indigo-650 text-white hover:bg-white hover:text-indigo-650 transition delay-75 ease-in-out cursor-pointer"
     >
-      Login / SignUp
+    <a href="/login">Login / SignUp</a>
     </div>
   );
 };
