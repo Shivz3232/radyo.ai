6e643a79280940ce58d46616a985e0d8f61b7ab2
gourav-nuzpapr <gourav@nuzpapr.com>
2021-10-05 08:59:33 +0530

    package.lock.json and userProfile Api updated

--- pages/api/userProfile.js
+++ pages/api/userProfile.js
@@ -6,7 +6,7 @@ import multerUpload from '../../utils/middleware/multer';
 import { uploads } from '../../utils/cloudinary';
 import path from 'path';
 
-const parser = new DataURIParser();
+var parser = new DataURIParser();
 
 const formatBufferto64 = file =>
   parser.format(path.extname(file.originalname).toString(), file.buffer);
@@ -21,22 +21,33 @@ const uploadMiddleware = multerUpload.single('profile_img');
 uploader.use(uploadMiddleware);
 
 uploader.post(async (req, res) => {
-  const file64 = formatBufferto64(req.file);
+  if (req.method === 'POST') {
+    try {
+      const file64 = formatBufferto64(req.file);
+      parser = null;
+      await uploads(file64.content, 'profile_images').then(uploadResult => {
+        let userProfile = new UserProfileModel({
+          email: req.body.email,
+          userName: req.body.fullName,
+          userId: req.body.userId,
+          about: req.body.about,
+          contact: req.body.contact,
+          img: uploadResult,
+        });
+        const saved = userProfile.save();
+      });
 
-  await uploads(file64.content, 'profile_images').then(uploadResult => {
-    let userProfile = new UserProfileModel({
-      email: req.body.email,
-      userName: req.body.fullName,
-      userId: req.body.userId,
-      about: req.body.about,
-      contact: req.body.contact,
-      img: uploadResult,
-    });
-    const saved = userProfile.save();
-  });
-  
-  res.status(200).json('Image Uploaded Successfully');
-  res.status(200).json({ data: 'success' });
+      res.status(200).json('Image Uploaded Successfully');
+      res.status(200).json({ data: 'success' });
+      res.end();
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+     res.status(405);
+     res.setHeader('Access-Control-Allow-Methods', 'GET');
+     res.end();
+  }
 });
 
 export default connect(uploader);
