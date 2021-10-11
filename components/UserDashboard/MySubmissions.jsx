581616c17b90a9e1a6aa2bcb34fc74b5c5299d24
gourav-nuzpapr <gourav@nuzpapr.com>
2021-10-11 16:13:08 +0530

    getMySubmissions Api added

--- components/UserDashboard/MySubmissions.jsx
+++ components/UserDashboard/MySubmissions.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AudioCards from '../AudioCard/AudioCards';
+import { useAuth } from '../../controllers/auth';
+import axios from 'axios';
 
 const mySubmissions = () => {
+  const { useremail } = useAuth();
+
+  useEffect(() => {
+    if (useremail) {
+      axios
+        .get(`/api/getMySubmissions/${useremail}`)
+        .then(response => console.log(response.data))
+        .catch(error => console.log(error));
+    }
+  }, []);
+
   return (
     <>
-      <h1>Hii</h1>
+      <h1 className="text-center">
+        If you have recently submitted an entry, it will be under review and
+        will appear here within 2 business days​.
+      </h1>
     </>
   );
 };
---
+++ pages/api/getMySubmissions/[id].js
@@ -0,0 +1,36 @@
+import connect from '../../../utils/middleware/mongoClient';
+import PodcastCreatorModel from '../../../models/podcastCreator';
+import PodcastModel from '../../../models/podcast';
+
+const getMySubmissions = async (req, res) => {
+  if (req.method === 'GET') {
+    const userProfile = await PodcastCreatorModel.find(
+      { email: req.query.id },
+      (error, result) => {
+        if (error) {
+          console.log(error);
+        }
+      }
+    );
+    console.log(userProfile[0]._id);
+    const podcast = await PodcastModel.find(
+      { creatorId: userProfile[0]._id },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(result);
+        }
+      }
+    );
+    console.log(podcast);
+    res.status(200);
+    res.end();
+  } else {
+    res.status(405);
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.end();
+  }
+};
+
+export default connect(getMySubmissions);
