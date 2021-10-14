d14e87af1b9a87dfdaabdfeace39628b0e1c2fd5
gourav-nuzpapr <gourav@nuzpapr.com>
2021-10-15 00:56:37 +0530

    user dashboard route is protected

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -1,14 +1,44 @@
 import React, { useState } from 'react';
 import Navbar from '../../components/UserDashboard/UserNavbar';
+import nookies from 'nookies';
+import { verifyIdToken } from '../../utils/firebase/firebaseAdmin';
+import { useRouter } from 'next/router';
 
-const dashboard = () => {
+const dashboard = ({redirect, props}) => {
+  const router = useRouter();
+  if (redirect) {
+    router.push(redirect.destination);
+  }
   return (
     <>
-        <div className="bg-gray-100">
-          <Navbar />
-        </div>
+      <div className="bg-gray-100">
+        <Navbar />
+      </div>
     </>
   );
+};
+
+export const getServerSideProps = async context => {
+  try {
+    const cookies = nookies.get(context);
+    const token = await verifyIdToken(cookies.token);
+    const { uid, email } = token;
+    if (uid) {
+      console.log('User authenticated');
+      return {
+        props: {},
+      };
+    }
+  } catch (err) {
+    console.log('User not authenticated');
+    return {
+      redirect: {
+        permanent: false,
+        destination: '/login',
+      },
+      props: {},
+    };
+  }
 };
 
 export default dashboard;
