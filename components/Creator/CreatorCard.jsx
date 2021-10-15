c02b93ac25e88f06673aa65824cdc0ffc46a1a46
prajakta-nuzpapr <prajakta@nuzpapr.com>
2021-10-15 14:26:22 +0530

    Changes updated

--- components/Creator/CreatorCard.jsx
+++ components/Creator/CreatorCard.jsx
@@ -8,7 +8,7 @@ const CreatorCard = ({ data }) => {
       <div className="creator-card--image">
             <img
               className="creator-card__roundedimage"
-              src={
+              src={ data &&
                 data.avatarImage
                   ? data.avatarImage :
                    '/lovebytes/images/Picture1.jpg'
@@ -20,10 +20,10 @@ const CreatorCard = ({ data }) => {
           
           <div className="creator-card__header--items">
             <div className="creator-card__header--item creator-card__author">
-              {data.creatorName}
+              {data && data.creatorName}
             </div>
             <div className="creator-card__header--item creator-card__aboutme">
-              {data.about}
+              {data && data.about}
             </div>
           </div>
         </div>
@@ -33,19 +33,19 @@ const CreatorCard = ({ data }) => {
       <div className="creatorCard__action--row">
         <div className="creator-card__action">
           <span className="creator-card__action--item">
-            {data.audiosPublished}
+            {data && data.audiosPublished}
           </span>
           <span className="creator-card__action--item">Audios </span>
         </div>
         <div className="creator-card__action">
           <span className="creator-card__action--item">
-            {data.playCount}
+            {data && data.playCount}
             </span>
           <span className="creator-card__action--item"> Plays</span>
         </div>
         <div className="creator-card__action">
           <span className="creator-card__action--item">
-            {data.subscriberCount}
+            {data && data.subscriberCount}
           </span>
           <span className="creator-card__action--item">Followers</span>
         </div>
