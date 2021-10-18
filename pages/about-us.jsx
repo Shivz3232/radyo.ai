a3741e4ebe13378fc8762f91d6780da0fb5e7e84
gourav-nuzpapr <gourav@nuzpapr.com>
2021-10-18 23:03:29 +0530

    Update about-us.jsx

--- pages/about-us.jsx
+++ pages/about-us.jsx
@@ -276,24 +276,4 @@ export function Prism() {
   );
 }
 
-// import React from 'react';
-
-// const AboutUs = () => {
-//   return (
-//     <>
-//       <div></div>
-//     </>
-//   );
-// };
-
-// export async function getStaticProps() {
-//   return {
-//     props: {
-//       activeTab: '/about-us',
-//     },
-//   };
-// }
-
-// export default AboutUs;
-
 export default AboutUs;
