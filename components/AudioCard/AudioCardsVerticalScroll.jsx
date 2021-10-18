7a818f146ccd9a794c3cd6c781b849802ae70b4d
Ritesh-Aggarwal <89374334+ritesh-agg@users.noreply.github.com>
2021-10-18 18:43:16 +0530

    Update AudioCardsVerticalScroll.jsx

--- components/AudioCard/AudioCardsVerticalScroll.jsx
+++ components/AudioCard/AudioCardsVerticalScroll.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { usePlaylist } from '../../controllers/PlaylistProvider';
 import NoResult from '../../assets/NoResultsFound.png';
 import AudioCard from './AudioCard';
@@ -17,13 +17,18 @@ const AudioCardsVerticalScroll = ({ audioCards }) => {
     for (var i = 0, j = 1; i < audioCards.length; i++, j++) {
       if (j % 5 === 0) {
         cards.push(
-          <div key={`google-card-${j+i}`} className="audio-card mini generic-card p-0">
+          <div
+            key={`google-card-${j + i}`}
+            className="audio-card mini generic-card p-0"
+          >
             <Adcard />
           </div>
         );
       }
 
-      cards.push(<AudioCard key={i} origin={origin} cardItemData={audioCards[i]} />);
+      cards.push(
+        <AudioCard key={i} origin={origin} cardItemData={audioCards[i]} />
+      );
     }
 
     return cards;
