acb9530cf094a42e8d7e35ac4f8443deca5adad4
aishnuz <91432335+aishnuz@users.noreply.github.com>
2021-10-21 01:42:19 +0530

    Update emailUtil.js

--- utils/emailUtil.js
+++ utils/emailUtil.js
@@ -1,21 +1,29 @@
 import axios from 'axios';
+import { AES, enc, lib } from 'crypto-js';
+
+const encryptMessage = message => {
+  var key = enc.Utf8.parse(process.env.EMAIL_MICROSERVICE_CLIENTID);
+  var iv = lib.WordArray.random(16);
+  var encrypted = AES.encrypt(JSON.stringify(message), key, { iv: iv });
+  return iv.concat(encrypted.ciphertext).toString(enc.Base64);
+};
 
 export const emailUtil = async (username, useremail, templatename) => {
   let payload = JSON.parse(process.env.EMAIL_MICROSERVICE_PAYLOAD_TEMPLATE);
   payload['username'] = username;
   payload['useremail'] = useremail;
   payload['template'] = templatename;
-
+  const data = { data: encryptMessage(payload) };
   await axios({
     method: 'POST',
     url: process.env.EMAIL_MICROSERVICE_ENDPOINT,
-    data: payload,
+    data: data,
     headers: { 'Content-Type': 'application/json' },
   })
     .then(res => {
       console.log(res);
     })
     .catch(error => {
-      console.log(error);
+      console.log('Error sending email trigger');
     });
 };
