4a1b86a9f0e24e3882a4d42ea1be530b77175a05
aishnuz <91432335+aishnuz@users.noreply.github.com>
2021-10-28 01:26:36 +0530

    Update playlist.js

--- controllers/playlist.js
+++ controllers/playlist.js
@@ -1,7 +1,7 @@
 import PlaylistModel from '../models/playlist';
 
 export const getAllPlaylists = async () => {
-  const allPlaylists = await PlaylistModel.find({})
+  const allPlaylists = await PlaylistModel.find({ podcastList: { $ne: [] } })
     .sort({ likeCount: -1 })
     .limit(15)
     .populate('podcastList')
